3c1ae96ad9b7aaa3959cca24e4f998e44872687e
Carlos Alvarado <calvarado@uchicago.edu>
2018-08-10 11:53:47 -0400

    archivos base Kattis

---
+++ Tareas/Tarea 2/Material adicional/engeng_base.py
@@ -0,0 +1,24 @@
+# Estructura de código para resolver problema de Kattis
+# https://open.kattis.com/problems/engineeringenglish
+
+import sys
+
+# Concentrarse en funcion "solve"; NO modificar otras partes del codigo
+
+def solve(lines):
+    '''
+    Input: lista de strings, cada uno correspondiente a una linea de input
+
+    Return: lista de strings, cada uno según se solicita en la
+    descripción del problema
+    '''
+    rv = []
+    
+    #MODIFICAR AQUI
+
+    return rv
+
+if __name__ == "__main__":
+    lines = [s.strip() for s in sys.stdin.readlines()]
+
+    print("\n".join(solve(lines)))
---
+++ Tareas/Tarea 2/Material adicional/r2_base.py
@@ -0,0 +1,22 @@
+# Estructura de código para resolver problema de Kattis
+# https://open.kattis.com/problems/r2
+
+import sys
+
+def get_r2(r1, s1):
+    '''
+    Dados dos numeros enteros (r1, s1), entrega otro numero entero r2
+    según se detalla en descripción del problema
+    '''
+    
+    #MODIFICAR AQUI
+    r2 = 0
+    #MODIFICAR AQUI
+
+    return r2
+
+if __name__ == "__main__":
+    
+    linea = sys.stdin.read()
+    
+    #MODIFICAR AQUI
